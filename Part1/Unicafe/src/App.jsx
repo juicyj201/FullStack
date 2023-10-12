4d51ca744da1cd99a45e06dc6003197514e838ca
Joshua Julies <JoshuaJ@bidvestil.com>
2023-10-12 14:29:43 +0200

    Added updates for calculations

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -6,28 +6,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-  const [avg, setAverage] = useState(0)
 
   const handleGoodClick = () => { 
     const updatedGood = good + 1
     setGood(updatedGood)
-    setAll(all + updatedGood)
+    setAll(all + 1)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    setAll(all + updatedNeutral)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
-    setAll(all + updatedBad)
-  }
-
-  const Average = () => {
-    
+    setAll(all + 1)
   }
 
   return (
@@ -58,17 +53,46 @@ const Button = ({handleClick, text}) => {
 }
 
 const Statistics = ({good, neutral, bad, all}) => {
-  return (
-    <>
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {avg}</p>
-      <p>Positive: {positive}</p>
-    </>
-  )
+  const avg = ((1*good)+(0*neutral)+(-1*bad))/all
+  const positive = (good/all*100)
+
+  if (all === 0) {
+    return(
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback has been given</p>
+      </>
+    ) 
+  } else {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <StatisticsLine text="Good" value={good}/>
+        <StatisticsLine text="Neutral" value={neutral}/>
+        <StatisticsLine text="Bad" value={bad}/>
+        <StatisticsLine text="All" value={all}/>
+        <StatisticsLine text="Average" value={avg}/>
+        <StatisticsLine text="Positive" value={positive}/>
+      </>
+    )
+  }
+}
+
+const StatisticsLine = ({text, value}) => {
+  if(text === "Positive"){
+    return(
+      <div>
+        <p>{text}: {value}%</p>
+      </div>
+    )
+  }
+  else{
+    return(
+      <div>
+        <p>{text}: {value}</p>
+      </div>
+    )
+  }
 }
 
 export default App
